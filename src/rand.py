561c3545b7e3293baf25f140106afdacbdd83ee4
Joel Jogy <joeljogy07@gmail.com>
2024-09-10 21:39:08 -0400

    Fix pyright errors

--- src/rand.py
+++ src/rand.py
@@ -1,10 +1,11 @@
 import subprocess
 
-
 def random_array(arr):
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    for i in range(0,len(arr)):
+        # Correcting the arguments passed to subprocess.run
+        shuffled_num = subprocess.run(["shuf", "-i", "1-20", "-n", "1"], capture_output=True, text=True)
+        
+        # Fixing int() conversion by specifying base as 10 and using decoded output
+        arr[i] = int(shuffled_num.stdout.strip(), base=10)  
     return arr
