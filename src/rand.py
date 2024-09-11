a3dcc6bc8cb8f689c73fc4c936e2951330da6808
Girish G N <girishgn12@gmail.com>
2024-09-10 22:21:30 -0400

    Fixing pylint errors

--- src/rand.py
+++ src/rand.py
@@ -1,11 +1,23 @@
+"""
+This module provides a function to generate a random array of integers.
+
+It includes the `random_array` function, which fills a given list with random integers
+between 1 and 20 using the `shuf` command-line utility.
+"""
+
 import subprocess
 
 def random_array(arr):
-    shuffled_num = None
-    for i in range(0,len(arr)):
-        # Correcting the arguments passed to subprocess.run
-        shuffled_num = subprocess.run(["shuf", "-i", "1-20", "-n", "1"], capture_output=True, text=True)
-        
-        # Fixing int() conversion by specifying base as 10 and using decoded output
-        arr[i] = int(shuffled_num.stdout.strip(), base=10)  
+    """
+    Fills the given list with random integers between 1 and 20.
+
+    :param arr: List to be filled with random integers.
+    :return: The list filled with random integers.
+    """
+    for i, _ in enumerate(arr):
+        # Running the subprocess command
+        result = subprocess.run(["shuf", "-i", "1-20", "-n", "1"],
+                                capture_output=True, text=True, check=True)
+        # Convert the output to integer, ensuring to strip any trailing newlines or spaces
+        arr[i] = int(result.stdout.strip(), base=10)  # Need to specify base as 10
     return arr
